8efe143da02fb7094b1e05ff02494778ffb2143b
Hao Zhu <haozhu@ifar-rstudio.hsl.harvard.edu>
2017-11-22 14:25:13 -0500

    initial commit

---
+++ install_packages.R
@@ -0,0 +1,28 @@
+# CRAN packages
+install.packages(
+  c("tidyverse", "ezsummary", 
+    "knitr", "rmarkdown", "shiny", "REDCapR",
+    "viridis", "sas7bdat", "devtools",
+    "bookdown", "DT", "shinythemes",
+    "highcharter", "ggthemes", 
+    "data.table", "DiagrammeR", "fastmatch",
+    "formatR", "gridExtra", "Hmisc", "grid",
+    "httr", "htmltools", "jsonlite", "lattice", "leaflet",
+    "mailR", "miniUI", "nlme", "pander", "pkgcopier", "purrr",
+    "quantmod", "rcrossref", "roxygen2",
+    "rticles", "scatterD3", "shinydashboard", "shinyjs",
+    "sparklyr",  "testthat", "xml2",
+    "timevis", "ggpmisc", "rcrossref", "geepack", "rpart", 
+    "deming", "gtools", "magrittr", 
+    "car", "plotly")
+)
+
+# Github
+devtools::install_github("hebrewseniorlife/prettymemo")
+devtools::install_github("hebrewseniorlife/coretools")
+
+# Bio conductor
+# Please uncomment the following code if you need to install 
+# packages from bioconductor in the future
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("") # Fill in the package name here
