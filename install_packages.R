6d178713db92d1f75a1a3d2fbe0aa5013968cee9
Hao Zhu <haozhu233@gmail.com>
2017-12-07 13:26:36 -0500

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -2,13 +2,16 @@
 
 # CRAN packages
 install.packages(
-  c("tidyverse", "bookdown", "broom", "car", "data.table", "deming", "devtools", "DiagrammeR", "DT", "ezsummary", "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", "ggthemes", "grid", "gridExtra", "gtools", "haven", "highcharter", "Hmisc", "htmltools", "httr", "irr", "jsonlite", "kable", "kableExtra", "knitr", "lattice", "leaflet", "MASS", "magrittr", "mailR", "miniUI", "nlme", "pander", "pkgcopier", "plotly", "purrr", "psych", "quantmod", "rcrossref", "REDCapR", "rmarkdown", "roxygen2", "rpart", "rticles", "sas7bdat", "scatterD3", "shiny", "shinydashboard", "shinyjs", "shinythemes", "sparklyr", "stringi", "survival", "testthat", "timevis", "viridis", "xml2" ),
+  c("tidyverse", "bookdown", "broom", "car", "data.table", "deming", "devtools", "DiagrammeR", "DT", "ezsummary", "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", "ggthemes", "grid", "gridExtra", "gtools", "haven", "highcharter", "Hmisc", "htmltools", "httr", "irr", "jsonlite", "kable", "kableExtra", "keras", "knitr", "lattice", "leaflet", "MASS", "magrittr", "mailR", "miniUI", "nlme", "pander", "pkgcopier", "plotly", "purrr", "psych", "quantmod", "rcrossref", "REDCapR", "rmarkdown", "roxygen2", "rpart", "rticles", "sas7bdat", "scatterD3", "shiny", "shinydashboard", "shinyjs", "shinythemes", "sparklyr", "stringi", "survival", "testthat", "timevis", "viridis", "xml2" ),
   repos = "https://cloud.r-project.org/"
 )
 
 # Github
 devtools::install_github("hebrewseniorlife/prettymemo")
 devtools::install_github("hebrewseniorlife/coretools")
+
+# Python libs
+keras::install_keras(extra_packages = c("pandas", "matplotlib", "seaborn"))
 
 # Bio conductor
 # Please uncomment the following code if you need to install 
