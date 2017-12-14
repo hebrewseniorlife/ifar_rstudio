d0fc9f23e7fa4d577836fd75c5cf7492bca695e0
Hao Zhu <haozhu233@gmail.com>
2017-12-14 14:34:20 -0500

    add servr

--- install_packages.R
+++ install_packages.R
@@ -2,7 +2,7 @@
 
 # CRAN packages
 install.packages(
-  c("tidyverse", "bookdown", "broom", "car", "data.table", "deming", "devtools", "DiagrammeR", "DT", "ezsummary", "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", "ggthemes", "grid", "gridExtra", "gtools", "haven", "highcharter", "Hmisc", "htmltools", "httr", "irr", "jsonlite", "kable", "kableExtra", "keras", "knitr", "lattice", "leaflet", "MASS", "magrittr", "mailR", "miniUI", "nlme", "pander", "pkgcopier", "plotly", "purrr", "psych", "quantmod", "rcrossref", "REDCapR", "rmarkdown", "roxygen2", "rpart", "rticles", "sas7bdat", "scatterD3", "shiny", "shinydashboard", "shinyjs", "shinythemes", "sparklyr", "stringi", "survival", "testthat", "timevis", "viridis", "xgboost", "xml2" ),
+  c("tidyverse", "bookdown", "broom", "car", "data.table", "deming", "devtools", "DiagrammeR", "DT", "ezsummary", "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", "ggthemes", "grid", "gridExtra", "gtools", "haven", "highcharter", "Hmisc", "htmltools", "httr", "irr", "jsonlite", "kable", "kableExtra", "keras", "knitr", "lattice", "leaflet", "MASS", "magrittr", "mailR", "miniUI", "nlme", "pander", "pkgcopier", "plotly", "purrr", "psych", "quantmod", "rcrossref", "REDCapR", "rmarkdown", "roxygen2", "rpart", "rticles", "sas7bdat", "scatterD3", "servr", "shiny", "shinydashboard", "shinyjs", "shinythemes", "sparklyr", "stringi", "survival", "testthat", "timevis", "viridis", "xgboost", "xml2" ),
   repos = "https://cloud.r-project.org/"
 )
 
