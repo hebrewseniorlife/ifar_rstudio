072c011b45e44a540b470d10f542142b49d03aa4
Hao Zhu <haozhu233@gmail.com>
2018-01-19 12:55:01 -0500

    change oro to neurobase

--- install_packages.R
+++ install_packages.R
@@ -2,7 +2,20 @@
 
 # CRAN packages
 install.packages(
-  c("tidyverse", "bookdown", "broom", "car", "caret", "data.table", "deming", "devtools", "DiagrammeR", "DT", "ezsummary", "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", "ggthemes", "grid", "gridExtra", "gtools", "haven", "highcharter", "Hmisc", "htmltools", "httr", "irr", "jsonlite", "kable", "kableExtra", "keras", "knitr", "lattice", "leaflet", "MASS", "magrittr", "mailR", "miniUI", "nlme", "oro.nifti", "pander", "pkgcopier", "plotly", "purrr", "psych", "quantmod", "rcrossref", "REDCapR", "rmarkdown", "ROCR", "roxygen2", "rpart", "rticles", "sas7bdat", "scatterD3", "servr", "shiny", "shinydashboard", "shinyjs", "shinythemes", "sparklyr", "stringi", "survival", "testthat", "timevis", "viridis", "xgboost", "xml2" ),
+  c("tidyverse", "bookdown", "broom", "car", "caret", "data.table", 
+    "deming", "devtools", "DiagrammeR", "DT", "ezsummary", 
+    "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", 
+    "ggthemes", "grid", "gridExtra", "gtools", "haven", 
+    "highcharter", "Hmisc", "htmltools", "httr", "irr", 
+    "jsonlite", "kable", "kableExtra", "keras", "knitr", 
+    "lattice", "leaflet", "MASS", "magrittr", "mailR", 
+    "miniUI", "neurobase", "nlme", "pander", "pkgcopier", 
+    "plotly", "purrr", "psych", "quantmod", "rcrossref", 
+    "REDCapR", "rmarkdown", "ROCR", "roxygen2", "rpart", 
+    "rticles", "sas7bdat", "scatterD3", "servr", "shiny", 
+    "shinydashboard", "shinyjs", "shinythemes", "sparklyr", 
+    "stringi", "survival", "testthat", "timevis", 
+    "viridis", "xgboost", "xml2" ),
   repos = "https://cloud.r-project.org/"
 )
 
