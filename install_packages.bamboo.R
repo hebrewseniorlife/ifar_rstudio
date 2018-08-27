bc1e4b2fdc8beba615ae02015a9e96b7b41275d4
Hao Zhu <haozhu233@gmail.com>
2018-08-27 15:27:41 -0400

    Update install_packages.bamboo.R

--- install_packages.bamboo.R
+++ install_packages.bamboo.R
@@ -1,5 +1,5 @@
 # Please search on this page before you put in new package names. 
-list.of.packages <- c("tidyverse", "bookdown", "broom", "blogdown", "car", "caret", "data.table", 
+list.of.packages <- c("tidyverse", "bookdown", "broom", "blogdown", "car", "caret", "citr", "data.table", 
     "deming", "devtools", "DiagrammeR", "DT", "dtplyr", "ezsummary", 
     "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", 
     "ggthemes", "grid", "gridExtra", "gtools", "haven", 
