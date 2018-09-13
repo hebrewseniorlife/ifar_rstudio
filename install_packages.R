01eb4bdf2bbb8fcc9ecce383d9b93f1595edd5bb
Hao Zhu <haozhu233@gmail.com>
2018-09-13 14:53:45 -0400

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -3,7 +3,7 @@
 # CRAN packages
 install.packages(
   c("tidyverse", "bookdown", "broom", "blogdown", "car", "caret", "citr", "data.table", 
-    "deming", "devtools", "DiagrammeR", "DT", "dtplyr", "ezsummary", 
+    "deming", "devtools", "DiagrammeR", "DT", "dtplyr", "ezsummary", "extrafont", 
     "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", 
     "ggthemes", "grid", "gridExtra", "gtools", "haven", 
     "highcharter", "Hmisc", "htmltools", "httr", "irr", 
