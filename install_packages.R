f91b6eccf752e55f3047d8a5312e8dee6d125c18
Hao Zhu <haozhu233@gmail.com>
2019-04-30 12:00:47 -0400

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -6,25 +6,25 @@ install.packages(
     "deming", "devtools", "DiagrammeR", "DT", "dtplyr", "extrafont", 
     "fastmatch", "formatR", "geepack", "ggpmisc", "ggridges", 
     "ggthemes", "grid", "gridExtra", "gtools", "haven", 
-    "highcharter", "Hmisc", "htmltools", "httr", "irr", 
-    "jsonlite", "kable", "kableExtra", "keras", "knitr", 
-    "lattice","lavaan", "leaflet", "MASS", "magrittr", "mailR", "mathpix", "meta", 
-    "miniUI", "MplusAutomation", "neurobase", "nlme", "odbc", "pander", "pkgcopier", 
+    "highcharter", "Hmisc", "htmltools", "httr", "irr", "jomo", 
+    "jsonlite", "kableExtra", "keras", "knitr", 
+    "lattice","lavaan", "leaflet", "MASS", "magrittr", "mailR", "mathpix", "meta", "mice", 
+    "miniUI", "mitml", "MplusAutomation", "neurobase", "nlme", "naniar",
+    "odbc", "ordinal", "pan", "pander", "pkgcopier", 
     "plotly", "polycor", "prettydoc", "purrr", "psych", "quantmod", "radix", 
-    "REDCapR", "rmarkdown", "ROCR", "roxygen2", "rpart", "RSQLite",
+    "REDCapR", "rmarkdown", "ROCR", "roxygen2", "rpart", "rpart.plot", "RSQLite",
     "rticles", "sas7bdat", "scatterD3", "servr", "shiny", 
-    "shinydashboard", "shinyjs", "shinythemes", "slackr", "sparklyr", 
-    "stringi", "survival", "testthat", "timevis", 
-    "viridis", "writexl", "xgboost", "xml2" ),
+    "shinydashboard", "shinyjs", "shinythemes","simputation", "slackr", "sparklyr", 
+    "stringi", "survival", "testthat", "timevis", "ucminf", "UpSetR",
+    "viridis", "visdat", "writexl", "xgboost", "xml2" ),
   repos = "https://cloud.r-project.org/"
 )
 
 # Github
 devtools::install_github("hebrewseniorlife/coretools")
-devtools::install_github("hebrewseniorlife/ro2")
+devtools::install_github("hebrewseniorlife/r2cluster")
 devtools::install_github("ropensci/rcrossref")
 devtools::install_github("r-lib/pkgdown")
-# devtools::install_github("thomasp85/transformr")
 devtools::install_github('thomasp85/gganimate')
 devtools::install_github("hebrewseniorlife/memor")
 
@@ -36,5 +36,4 @@ devtools::install_github("hebrewseniorlife/memor")
 
 # Install local package
 devtools::install_git("ssh://git@ifar-stash.hsl.harvard.edu:7999/irp/mobilityandfall.git", credentials = git2r::cred_ssh_key())
-
-
+devtools::install_git("ssh://git@ifar-stash.hsl.harvard.edu:7999/irp/marcus.git", credentials = git2r::cred_ssh_key())
