be623f4afdb9268c4e95364529b785325e9ee52f
Hao Zhu <haozhu233@gmail.com>
2019-06-17 11:55:43 -0400

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -2,7 +2,7 @@
 
 # CRAN packages
 install.packages(
-  c("tidyverse", "tidymodels", "bookdown", "broom", "blogdown", "car", "caret", "citr", "data.table", 
+  c("tidyverse", "bookdown", "broom", "blogdown", "car", "caret", "citr", "data.table", 
     "deming", "devtools", "DiagrammeR", "DT", "dtplyr", "extrafont", 
     "fastmatch", "flexdashboard", "formatR", "geepack", "ggpmisc", "ggridges", 
     "ggthemes", "grid", "gridExtra", "gtools", "haven", 
